83602ce663e1c947812089d2bf4a04d19ddf463a
Sylvain Metayer <sylvain_metayer@outlook.fr>
2016-02-26 17:10:24 +0100

    Continuité + passage test.php en local

--- classes/CompteManager.class.php
+++ classes/CompteManager.class.php
@@ -8,51 +8,66 @@ class CompteManager{
 
   /**
   * Permet d'insérer un compte dans la base de données, avec un solde nul.
-  *@return la valeur de l'id du compte.
+  *@return idInsere en base
+  * Méthode testée et fonctionnelle
   */
   public function add($compte){
     $requete=$this->db->prepare(
       'INSERT INTO compte(solde) VALUES (:solde)'
     );
 
-    $requete->bindValue(':solde', 0);
+    $requete->bindValue(':solde',$compte->getSolde() != null ? $compte->getSolde() : 0);
 
     $requete->execute();
 
-    $idInserted = $db->lastInsertId();
+    $idInserted = $this->db->lastInsertId();
 
     return $idInserted;
   }
 
   /**
   * Permet de créditer le solde d'un compte avec un montant strictement positif.
-  *@return true si succès, false sinon.
+  *@return {'idCompte' : x, "creditOK": true|false}
+  *
   */
   public function crediter($montant, $idCompte) {
     if ($montant <= 0) {
       return false;
     }
 
-    $requete=$this->db->prepare(
-      'UPDATE compte SET solde = solde + :montant where idCompte = :idCompte;'
-    );
+    //Test a faire si le compte existe ou pas.
+
+    $sql = 'UPDATE compte SET solde = solde + :montant where idCompte = :idCompte;';
+    $requete=$this->db->prepare($sql);
 
     $requete->bindValue(':montant', $montant);
     $requete->bindValue(':idCompte', $idCompte);
 
     $retour = $requete->execute();
 
-    if ($retour != 0) {
-      return false;
+    var_dump($requete);
+    echo $retour;
+    if ($retour == 0) {
+      $response = array(
+        'idCompte' => $idCompte,
+        'creditOK' => false
+      );
     }
 
-    return true;
+    $response = array(
+      'idCompte' => $idCompte,
+      'creditOK' => true
+    );
+
+    $json = json_encode($response);
+    return $json;
   }
 
 
   /**
   * Retourne le solde d'un client.
   *@return un objet JSON au format {idCompte : id | null, solde : solde | null}
+  * Méthode testée et fonctionnelle.
   */
   public function getSolde($idCompte) {
 
@@ -69,7 +84,7 @@ class CompteManager{
     }
 
     $tmp = $requete->fetch(PDO::FETCH_ASSOC);
-    
+
     $compte = new Compte($tmp);
 
     $response = array(
@@ -77,8 +92,10 @@ class CompteManager{
                   'solde' => $compte->getSolde()
                 );
 
-    $jsonstring = json_encode($response);
-    return $jsonstring;
+    $json = json_encode($response);
+    return $json;
   }
+
+
 }
 ?>
--- include/pages/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-$db = new Mypdo();
-
-$compteManager = new CompteManager($db);
-$resu = $compteManager->getSolde(1);
-echo $resu;
-?>
-<h1>Liste des villes</h1>
