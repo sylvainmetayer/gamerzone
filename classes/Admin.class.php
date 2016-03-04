c247fc51b8231b3320ea6c2b7e3792b5279e1e4c
Sylvain Metayer <sylvain_metayer@outlook.fr>
2016-03-04 14:12:41 +0100

    Ajout Connexion Admin

---
+++ classes/Admin.class.php
@@ -0,0 +1,38 @@
+<?php
+
+class Admin{
+
+  private $login;
+  private $pwd;
+
+  public function __construct($valeurs = array()){
+    $this->affecte($valeurs);
+  }
+
+  public function affecte($donnees){
+    foreach($donnees as $attribut => $valeur){
+      switch($attribut) {
+        case 'login' : $this->setLogin($valeur); break;
+        case 'pwd' : $this->setPwd($valeur); break;
+      }
+    }
+  }
+
+  public function getLogin() {
+    return $this->login;
+  }
+
+  public function getPwd() {
+    return $this->pwd;
+  }
+
+  public function setPwd($pwd) {
+    $this->pwd = $pwd;
+  }
+
+  public function setLogin($login) {
+    $this->login = $login;
+  }
+
+}
+?>
