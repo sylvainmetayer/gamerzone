9489fd215084d387213416d2339c88898d327d99
Sylvain Metayer <sylvain_metayer@outlook.fr>
2016-03-04 16:10:27 +0100

    delete client OK + nbClient

--- classes/ClientManager.class.php
+++ classes/ClientManager.class.php
@@ -11,9 +11,15 @@ class ClientManager
 
     public function add($client)
     {
-        $compte = new Compte(array('solde' => 0));
-        $compteManager = new CompteManager($this->db);
-        $idCompte = $compteManager->add($compte);
+        if ($client->getIdCompte() != null) {
+            $idCompte = $client->getIdCompte();
+        } else {
+            $compte = new Compte(array('solde' => 0));
+            $compteManager = new CompteManager($this->db);
+            $idCompte = $compteManager->add($compte);
+        }
+
+        $date = getEnglishDate($client->getDate_naissance());
 
         $sql = 'INSERT INTO client(idCompte, nom, prenom, mail, date_naissance, login, pwd) VALUES (:idCompte, :nom, :prenom, :mail, :date_naissance, :login, :pwd)';
 
@@ -23,10 +29,48 @@ class ClientManager
         $requete->bindValue(':nom', $client->getNom());
         $requete->bindValue(':prenom', $client->getPrenom());
         $requete->bindValue(':mail', $client->getMail());
-        $requete->bindValue(':date_naissance', $client->getDate_naissance());
+        $requete->bindValue(':date_naissance', $date);
         $requete->bindValue(':login', $client->getLogin());
         $requete->bindValue(':pwd', $client->getPwd());
 
-        return $requete->execute();
+        $retour = $requete->execute();
+        $idInserted = $this->db->lastInsertId();
+
+        $response = array(
+          'idClient' => $idInserted,
+          'idCompte' => $idCompte,
+          'ajoutClient' => $retour,
+        );
+
+        return json_encode($response);
+    }
+
+    public function getNbClient()
+    {
+        $sql = 'SELECT count(idClient) as nb FROM client;';
+        $requete = $this->db->prepare($sql);
+        $retour = $requete->execute();
+        $data = $requete->fetch(PDO::FETCH_ASSOC)['nb'];
+
+        $response = array(
+          'nbClient' => $data,
+        );
+
+        return json_encode($response);
+    }
+
+    public function delete($client)
+    {
+        $sql = 'DELETE FROM client WHERE idClient = :idClient';
+        $requete = $this->db->prepare($sql);
+
+        $requete->bindValue(':idClient', $client->getIdClient());
+        $retour = $requete->execute();
+        $response = array(
+          'idClient' => $client->getIdClient(),
+          'delete' => $retour,
+        );
+
+        return json_encode($response);
     }
 }
